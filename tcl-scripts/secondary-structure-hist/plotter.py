a986481f3f273cc1fb30bb1ba4c44eaacb64aa1a
Gideon Mitchell <gideonm@uchicago.edu>
2023-06-20 16:10:08 -0500

    Fix plotting bugs with 42-residue chains

--- tcl-scripts/secondary-structure-hist/plotter.py
+++ tcl-scripts/secondary-structure-hist/plotter.py
@@ -5,10 +5,11 @@ from matplotlib import pyplot as plt
 import os
 
 abeta_sequence = ['ASP', 'ALA', 'GLU', 'PHE', 'ARG', 'HIS', 'ASP', 'SER', 'GLY', 'TYR', 'GLU', 'VAL', 'HIS', 'HIS', 'GLN', 'LYS', 'LEU', 'VAL', 'PHE', 'PHE', 'ALA', 'GLU', 'ASP', 'VAL', 'GLY', 'SER', 'ASN', 'LYS', 'GLY', 'ALA', 'ILE', 'ILE', 'GLY', 'LEU', 'MET', 'VAL', 'GLY', 'GLY', 'VAL', 'VAL', 'ILE', 'ALA']
-abeta_sequence = "".join(abeta_sequence)
-csv_dir = "abeta-csv"
+abeta_str = "".join(abeta_sequence)
+csv_dir = "abeta-fibril-csv"
 residue_count = 42
 total_chain_count = 0
+long_chain_chount = 0
 
 alpha = np.zeros(residue_count, dtype=int)
 beta  = np.zeros(residue_count, dtype=int)
@@ -27,19 +28,21 @@ for fname in os.listdir(csv_dir):
         
         # check if the current chain is an ABeta monomer, sequence its structure if it is
         aa_sequence = list(chain["Residue Name"])
-        if "".join(aa_sequence) in abeta_sequence:
+        if "".join(aa_sequence) in abeta_str:
             print(f"Sequencing {infile} Chain {cname}")
             
             total_chain_count += 1
+            if len(aa_sequence) == 42:
+                long_chain_chount += 1
 
-            # TODO: POSSIBLE BUG here
             chain_start = int(min(chain["PDB ResID"]))
             chain_stop  = int(max(chain["PDB ResID"]))
 
             # iterate through each residue in the chain. If it is in the structure,
             # add it to out sequence
-            for resid in range(chain_start, chain_stop):
-                # select residue by resid
+            for res_index in range(residue_count):
+                # select data by residueID. Note that this is 1-indexed
+                resid = res_index + 1
                 residue_data = chain.loc[chain["PDB ResID"] == resid]
 
                 if not residue_data.empty:
@@ -47,35 +50,42 @@ for fname in os.listdir(csv_dir):
                     
                     # alpha-helix
                     if structure in ["H", "G", "I"]:
-                        alpha[resid-1] += 1
+                        alpha[res_index] += 1
                     
                     # beta strand or sheet
                     elif structure in ["B", "b", "E"]:
-                        beta[resid-1] += 1
+                        beta[res_index] += 1
                     
                     # turn
                     elif structure in ["T"]:
-                        turn[resid-1] += 1
+                        turn[res_index] += 1
 
                     # other, listed as a coil
                     else:
-                        other[resid-1] += 1
+                        other[res_index] += 1
                 
                 else:
-                    other[resid-1] += 1
+                    other[res_index] += 1
 
 
-legend_dict = {"Alpha": alpha, "Beta": beta, "Turn": turn, "Other": other}
+legend_dict = {"Alpha": alpha, "Beta": beta, "Turn": turn}
 
 x = np.arange(residue_count)  
 width = 0.25  # the width of the bars
 multiplier = 3
 
 fig, ax = plt.subplots(layout='constrained')
+plt.xticks(np.arange(1,43, dtype=int), abeta_sequence, rotation=-60)
 
-for attribute, measurement in legend_dict.items():
+# add line at 50%
+plt.axhline(y=0.2, color='r', linestyle='-')
+
+for index, (attribute, measurement) in enumerate(legend_dict.items()):
     # normalize over total numeber of chains
-    measurement = measurement / total_chain_count
+    if index < 40:
+        measurement = measurement / total_chain_count
+    else:
+        measurement = measurement / long_chain_chount
 
     offset = width * multiplier
     rects = ax.bar(x + offset, measurement, width, label=attribute)
@@ -84,7 +94,7 @@ for attribute, measurement in legend_dict.items():
 ax.set_ylabel("Frequency of Secondary Structure")
 ax.set_xlabel("Residue ID")
 ax.set_title("Frequency of Secondary Structure per Residue of ABeta-Monomer")
-ax.legend(loc='upper right', ncols=2)
+ax.legend(loc='upper right')
 
 plt.show()
 
